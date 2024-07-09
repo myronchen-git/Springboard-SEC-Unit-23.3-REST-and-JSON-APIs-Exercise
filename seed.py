bed71e43e170365c84153bc71c9d17c5b3643ffc
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-07-09 13:01:25 -0700

    fix: update seed.py for new app initialization

--- seed.py
+++ seed.py
@@ -1,22 +1,27 @@
-from app import app
-from models import db, Cupcake
+from app import create_app
+from models import Cupcake, connect_db, db
 
+# ==================================================
 
-db.drop_all()
-db.create_all()
+app = create_app("cupcakes")
+connect_db(app)
 
-c1 = Cupcake(
-    flavor="cherry",
-    size="large",
-    rating=5,
-)
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
-c2 = Cupcake(
-    flavor="chocolate",
-    size="small",
-    rating=9,
-    image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
-)
+    c1 = Cupcake(
+        flavor="cherry",
+        size="large",
+        rating=5,
+    )
 
-db.session.add_all([c1, c2])
-db.session.commit()
+    c2 = Cupcake(
+        flavor="chocolate",
+        size="small",
+        rating=9,
+        image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
+    )
+
+    db.session.add_all([c1, c2])
+    db.session.commit()
